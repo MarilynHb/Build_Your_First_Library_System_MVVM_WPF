fea7f872786724cedccfdc7eb52e7e9e510ca94a
Marilyn Haber <mha@logisoft.io>
2024-08-07 21:37:04 +0300

    Initialize Solution And Projects Setup

---
+++ src/Library.View/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Library.View;
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
+
